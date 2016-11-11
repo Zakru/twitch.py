b4986a020383a2e642f12a40d3ec0215e6df6916
Zakru <sakari.leukkunen@gmail.com>
2016-11-11 07:54:51 +0200

    Removed useless 'docs' folder

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:50,filenames:["index","thing"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,sampl:1,search:0,text:1},titles:["Welcome to twitch.py&#8217;s documentation!","&lt;no title&gt;"],titleterms:{document:0,indice:0,tabl:0,twitch:0,welcom:0}})
